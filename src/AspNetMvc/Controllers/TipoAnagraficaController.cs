b998abd978ebeb5885e464a111e3b1638b241159
Antonio Narra <tony@MacBook-Pro-di-Antonio.local>
2018-11-13 21:45:18 +0100

    Edited TipoAnagraficaController

--- src/AspNetMvc/Controllers/TipoAnagraficaController.cs
+++ src/AspNetMvc/Controllers/TipoAnagraficaController.cs
@@ -8,138 +8,100 @@ using Microsoft.EntityFrameworkCore;
 using Models;
 using Models.Tabelle;
 using Models.Views;
-
-namespace AspNetMvc.Controllers
-{
-    public class TipoAnagraficaController : BaseController
-    {
-
+namespace AspNetMvc.Controllers {
+    public class TipoAnagraficaController : BaseController {
         public TipoAnagraficaController(AnagraficaContext context) :base(context){}
         // GET: TipoAnagrafica
-        public async Task<IActionResult> Index()
-        {
+        public async Task<IActionResult> Index() {
             return View(await GetConvertedListAsync());
         }
-
         // GET: TipoAnagrafica/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> Details(int? id) {
+            if (id == null){
                 return NotFound();
             }
-
-            var tipoAnagrafica = await db.TipoAnagrafica
-                .FirstOrDefaultAsync(m => m.TipoAnagraficaId == id);
-            if (tipoAnagrafica == null)
-            {
+            var item = await db.TipoAnagrafica.FirstOrDefaultAsync(m => m.TipoAnagraficaId == id);
+            if (item == null){
                 return NotFound();
             }
-            var tpAnagrafica = new TipoAnagraficaView(tipoAnagrafica);
+            var tpAnagrafica = new TipoAnagraficaView(item);
             return View(tpAnagrafica);
         }
-
         // GET: TipoAnagrafica/Create
-        public IActionResult Create()
-        {
+        public IActionResult Create(){
             return View();
         }
-
         // POST: TipoAnagrafica/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TipoAnagraficaId,Descrizione")] TipoAnagrafica tipoAnagrafica)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Add(tipoAnagrafica);
+        public async Task<IActionResult> Create([Bind("TipoAnagraficaId,Descrizione")] TipoAnagraficaView view){
+            var item = ConvertFromView(view);
+            if (ModelState.IsValid){
+
+                db.Add(item);
                 await db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            var tpAnagrafica = new TipoAnagraficaView(tipoAnagrafica);
-            return View(tpAnagrafica);
+            var newView = new TipoAnagraficaView(item);
+            return View(newView);
         }
-
         // GET: TipoAnagrafica/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> Edit(int? id){
+            if (id == null){
                 return NotFound();
             }
-
-            var tipoAnagrafica = await db.TipoAnagrafica.FindAsync(id);
-            if (tipoAnagrafica == null)
-            {
+            var item = await db.TipoAnagrafica.FindAsync(id);
+            if (item == null){
                 return NotFound();
             }
-            var tpAnagrafica = new TipoAnagraficaView(tipoAnagrafica);
-            return View(tpAnagrafica);
+            var view = new TipoAnagraficaView(item);
+            return View(view);
         }
-
         // POST: TipoAnagrafica/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TipoAnagraficaId,Descrizione")] TipoAnagrafica tipoAnagrafica)
-        {
-            if (id != tipoAnagrafica.TipoAnagraficaId)
-            {
+        public async Task<IActionResult> Edit(int id, [Bind("TipoAnagraficaId,Descrizione")] TipoAnagraficaView view){
+            if (id != view.TipoAnagraficaId){
                 return NotFound();
             }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    db.Update(tipoAnagrafica);
+            if (ModelState.IsValid){
+                try {
+                    var item = ConvertFromView(view);
+                    db.Update(item);
                     await db.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TipoAnagraficaExists(tipoAnagrafica.TipoAnagraficaId))
-                    {
+                } catch (DbUpdateConcurrencyException){
+                    if (!TipoAnagraficaExists(view.TipoAnagraficaId)){
                         return NotFound();
-                    }
-                    else
-                    {
+                    } else {
                         throw;
                     }
                 }
                 return RedirectToAction(nameof(Index));
             }
-            var tpAnagrafica = new TipoAnagraficaView(tipoAnagrafica);
-            return View(tpAnagrafica);
+            return View(view);
         }
-
         // GET: TipoAnagrafica/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> Delete(int? id){
+            if (id == null){
                 return NotFound();
             }
-
-            var tipoAnagrafica = await db.TipoAnagrafica
-                .FirstOrDefaultAsync(m => m.TipoAnagraficaId == id);
-            if (tipoAnagrafica == null)
-            {
+            var item = await db.TipoAnagrafica.FirstOrDefaultAsync(m => m.TipoAnagraficaId == id);
+            if (item == null){
                 return NotFound();
             }
-            var tpAnagrafica = new TipoAnagraficaView(tipoAnagrafica);
-            return View(tpAnagrafica);
+            var view = new TipoAnagraficaView(item);
+            return View(view);
         }
-
         // POST: TipoAnagrafica/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var tipoAnagrafica = await db.TipoAnagrafica.FindAsync(id);
-            db.TipoAnagrafica.Remove(tipoAnagrafica);
+        public async Task<IActionResult> DeleteConfirmed(int id){
+            var item = await db.TipoAnagrafica.FindAsync(id);
+            db.TipoAnagrafica.Remove(item);
             await db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
@@ -151,9 +113,14 @@ namespace AspNetMvc.Controllers
             }
             return lst;
         }
-        private bool TipoAnagraficaExists(int id)
-        {
+        private bool TipoAnagraficaExists(int id){
             return db.TipoAnagrafica.Any(e => e.TipoAnagraficaId == id);
+        }
+        private TipoAnagrafica ConvertFromView(TipoAnagraficaView v){
+            TipoAnagrafica t = new TipoAnagrafica();
+            t.TipoAnagraficaId = v.TipoAnagraficaId;
+            t.Descrizione =v.Descrizione;
+            return t;
         }
     }
 }
